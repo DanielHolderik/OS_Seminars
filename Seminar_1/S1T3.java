120c8479d071acb5b081eaec461505ae37ca86e9
Daniel <danielholderik@gmail.com>
2024-09-08 20:58:10 +0200

    start T3

---
+++ Seminar_1/S1T3.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class S1T3 implements Runnable {
+    
+
+    @Override
+    public void run() {
+    long startTime = System.currentTimeMillis();
+    double timeElapsedSeconds = (System.currentTimeMillis() - startTime) /1000; 
+    } 
+
+    public static void main(String[] args) {
+    
+    }
+}
