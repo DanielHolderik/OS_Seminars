a918b0a04e9e4e7a50631c2ea31bfd7931df5566
Daniel <danielholderik@gmail.com>
2024-09-11 19:56:16 +0200

    T3 test

--- Seminar_1/S1T3.java
+++ Seminar_1/S1T3.java
@@ -2,16 +2,31 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 
-public class S1T3 implements Runnable {
+public class S1T3 implements Runnable{
     
 
     @Override
-    public void run() {
-    long startTime = System.currentTimeMillis();
-    double timeElapsedSeconds = (System.currentTimeMillis() - startTime) /1000; 
+    public void run(){
+    long currentTime = System.currentTimeMillis();
+    double timeElapsedSeconds = (System.currentTimeMillis() - currentTime) /1000; 
+
+        while (timeElapsedSeconds < 60){
+            try {
+                Thread.sleep(10);
+                timeElapsedSeconds += 0.01;
+                System.out.println("Elapsed Time:" + timeElapsedSeconds + "\n");
+
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
+            }
+        }
+        System.out.println("Stopwatch reached 60 seconds, joining threads... ");
     } 
 
-    public static void main(String[] args) {
-    
+    public static void main(String[] args){
+
+    S1T3 stopWatch = new S1T3();
+    Thread thread = new Thread(stopWatch);
+    thread.start();
     }
 }
