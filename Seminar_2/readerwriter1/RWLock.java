c4f077acec29de47f81eb8cadaee1c7851e6218a
Daniel <danielholderik@gmail.com>
2024-09-17 18:20:36 +0200

    task 1 done

--- Seminar_2/readerwriter1/RWLock.java
+++ Seminar_2/readerwriter1/RWLock.java
@@ -1,25 +1,46 @@
 package readerwriter1;
 
 public class RWLock {
-
+    int currentReaders = 0;
+    boolean isWriting = false;
+    int writersQueue = 0;
     public RWLock(){
-
+        // int currentReaders;
+        // boolean isWriting;
+        // int writersQueue;
     }
 
-    public void acquireRead(){
-
+    public synchronized void acquireRead(){
+        while (isWriting || writersQueue > 0){
+            try {
+                wait();
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        }currentReaders ++;
+    }
+    public synchronized void releaseRead(){
+        currentReaders --;
+        if (currentReaders == 0){
+            notify();
+        }
     }
 
-    public void acquireWrite(){
-
+    public synchronized void acquireWrite(){
+        writersQueue ++;
+        while (currentReaders > 0 || isWriting == true){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } 
+        }writersQueue --;
+         isWriting = true;
     }
 
-    public void releaseRead(){
-
-    }
-
-    public void releaseWrite(){
-
+    public synchronized void releaseWrite(){
+        isWriting = false;
+        notify();
     }
 
 }
